539f8472b73496e815cd8ca14c4408ddfac9658f
sabaka-chabaka <gamekimg360@gmail.com>
2026-03-01 19:04:26 +0400

    Initial commit

---
+++ SabakaBrowser/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SabakaBrowser;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
